2edc33028b49383a142dc17f0e9c5d6b1048f085
infinity_pain <94642389+slipperss@users.noreply.github.com>
2022-06-23 17:30:46 +0300

    Update settings.py

--- django_movie/settings.py
+++ django_movie/settings.py
@@ -61,24 +61,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-
-    'social_django.middleware.SocialAuthExceptionMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-
-
-
-
-
-
 
 
 ROOT_URLCONF = 'django_movie.urls'
